830336922928205b923035b631f32a06b5ccafb7
AntoineDidisheim <antoinedidisheim@gmail.com>
2020-05-30 16:28:30 +0200

    update for package push

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ setup(
   name = 'didipack',
   packages = ['didipack'],
   version = '0.1',
-  license='afl-3.0',
+  license='MIT',
   description = 'Usfull time saver for data-science and finance with python',
   author = 'Antoine Didisheim',
   author_email = 'antoinedidisheim@gmail.com',
   url = 'https://github.com/AntoineDidisheim',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/AntoineDidisheim/didipack/archive/v01.tar.gz',    # I explain this later on
   keywords = ['LaTex', 'Finance'],
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'pandas',
+          'statsmodels'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',
+    'Development Status :: MIT License',
     'Intended Audience :: Finance',      # Define that your audience are developers
     'Topic :: Data Science',
     'License :: OSI Approved :: Academic Free License v3.0',   # Again, pick a license
