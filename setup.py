c9bc2bf1f3a6620edfc7de5a8ca6259cf235013d
AntoineDidisheim <antoinedidisheim@gmail.com>
2023-08-13 19:10:12 +1000

    new version with yms

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='didipack',
     packages=setuptools.find_packages(),  # Automatically discover and include all packages in the package directory
-    version='4.0.5',
+    version='4.0.6',
     license='MIT',
     setup_requires=['wheel'],
     description='Usefull time saver for data-science and finance with python',
