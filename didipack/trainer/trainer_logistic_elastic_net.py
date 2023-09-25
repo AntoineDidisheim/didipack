a6a5d2e6652785bd1a373815259bf05eb9be5738
AntoineDidisheim <antoinedidisheim@gmail.com>
2023-09-25 16:48:14 +1000

    fixed predict proba to make the columns match

--- didipack/trainer/trainer_logistic_elastic_net.py
+++ didipack/trainer/trainer_logistic_elastic_net.py
@@ -17,7 +17,7 @@ class TrainerLogisticElasticNet(BaseTrainer):
 
 
     def _predict(self, x):
-        return self.m.predict_proba(x)
+        return self.m.predict_proba(x)[:,-1]
 
     def _train_model(self, x: pd.DataFrame, y: pd.DataFrame, hyper_params):
         self.m = LogisticRegression(C=hyper_params['shrinkage'][0], penalty='elasticnet', l1_ratio=hyper_params['l1_ratio'][0], solver='saga', fit_intercept=True, n_jobs=self.para)
