e358cf2cd735d3ef3385accd3693dd4cfddc731b
AntoineDidisheim <antoinedidisheim@gmail.com>
2023-11-06 14:22:34 +1100

    fix cluster to be a non date

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='didipack',
     packages=setuptools.find_packages(),  # Automatically discover and include all packages in the package directory
-    version='4.3.10',
+    version='4.3.2',
     license='MIT',
     setup_requires=['wheel'],
     description='Usefull time saver for data-science and finance with python',
