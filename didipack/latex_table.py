4a15e680e7850eef8833b0537dac264e41c15565
Ruoqi Guan <angelaguanau@gmail.com>
2023-11-29 01:46:03 +1100

    - Added support for PanelOLS - Fixed a bug where show_obs = False has no effect - Fixed a bug where setting bottom_block would replace nobs and r_squared

--- didipack/latex_table.py
+++ didipack/latex_table.py
@@ -1,16 +1,58 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+from linearmodels.panel.results import PanelEffectsResults
 from enum import Enum
+
 
 class ParValue(Enum):
     TSTAT = 1
     PVALUE = 2
     STD = 3
 
+
+class RegResult:
+    """
+    This class is used to create consistent results object and set profiles across different regression packages
+    """
+    def __init__(self, reg):
+        self.attributes = {}
+        self.reg = reg
+        self.rsqured_values = {}
+
+        self.regression_types = {
+            sm.regression.linear_model.RegressionResultsWrapper: self.init_statsmodels,
+            PanelEffectsResults: self.init_linearmodels_panel,
+        }
+        self.reg_type = type(self.reg)
+        if self.reg_type in self.regression_types:
+            self.regression_types[self.reg_type]()
+        else:
+            raise ValueError("The provided model is not supported.")
+
+    def __getattr__(self, item):
+        """
+        This function is used to get the value of an attribute of the regression object.
+        """
+        return self.attributes[item] if item in self.attributes else getattr(self.reg, item)
+
+    def init_statsmodels(self):
+        self.rsqured_values = {r'$R^2$': self.rsquared_adj} \
+            if hasattr(self, 'rsquared_adj') else {'Pseudo $R^2$': self.prsquared}
+
+    def init_linearmodels_panel(self):
+        self.attributes['tvalues'] = self.reg.tstats
+        self.attributes['bse'] = self.reg.std_errors
+        self.rsqured_values = {
+            r'$R^2$': self.rsquared,
+            r'$R^2_{\text{between}}$': self.rsquared_between,
+            r'$R^2_{\text{overall}}$': self.rsquared_overall,
+        }
+
+
 class OneReg:
     def __init__(self, reg, show_list=[], hide_list=[], blocks=[], bottom_blocks=[]):
-        self.reg = reg
+        self.reg = RegResult(reg)
         if show_list == []:
             self.show_list = []
             for s in self.reg.params.keys():
@@ -51,54 +93,40 @@ class OneReg:
                 t = abs(v_l[1] - p_l[1])
                 t = r'\phantom{' + '*' * t + '}'
                 if v_l[1] > p_l[1]:
-                    p = p+t
+                    p = p + t
                 if v_l[1] < p_l[1]:
-                    v = v+t
+                    v = v + t
                 # else:
-                    # p = r'\phantom{(}' + p + r'\phantom{)}'
+                # p = r'\phantom{(}' + p + r'\phantom{)}'
                 d[k] = p
 
                 t = pd.Series(dtype=object)
-                t[''] =v
+                t[''] = v
 
-                d = pd.concat([d,t],axis=0)
-                
+                d = pd.concat([d, t], axis=0)
+
             else:
                 t = pd.Series(dtype=object)
                 t[k] = TableReg.missing_symbol
                 t[''] = TableReg.missing_symbol
-                d = pd.concat([d,t],axis=0)
+                d = pd.concat([d, t], axis=0)
+
         # now we can add the "blocks", that is fix effects and others
-        for block in self.blocks:
+        summary_block = {}
+        if TableReg.show_obs:
+            summary_block['Observations'] = f'{int(self.reg.nobs):,}'
+        if TableReg.show_r2:
+            summary_block.update({k: np.round(v, TableReg.round_r2) for k, v in self.reg.rsqured_values.items()})
+
+        all_blocks = self.blocks + [summary_block] + self.bottom_block
+
+        for block in all_blocks:
             t = pd.Series(dtype=object)
             t[TableReg.group_key] = ''
 
             for k in block.keys():
                 t[k] = block[k]
-            d = pd.concat([d,t],axis=0)
-
-        # finaly additional info (r² and n.obs per default, but you can add anything through bottom blocks
-        if TableReg.show_obs | TableReg.show_r2 | (len(self.bottom_block)>0):
-            t = pd.Series(dtype=object)
-            t[TableReg.group_key] = ''
-            t['Observations'] = f'{int(self.reg.nobs):,}'
-
-            if hasattr(self.reg,'rsquared_adj'):
-
-                t[r'$R^2$'] = np.round(self.reg.rsquared_adj,TableReg.round_r2)
-            else:
-                t[r'Pseudo $R^2$'] = np.round(self.reg.prsquared,TableReg.round_r2)
-
-            first_block = True
-            for block in self.bottom_block:
-                if first_block:
-                    first_block = False
-                else:
-                    t[TableReg.group_key] = ''
-                t = pd.Series(dtype=object)
-                for k in block.keys():
-                    t[k] = block[k]
-            d = pd.concat([d,t],axis=0)
+            d = pd.concat([d, t], axis=0)
         return d
 
 
@@ -142,12 +170,9 @@ class TableReg:
         if 'rename_dict' in option:
             self.set_rename_dict(option['rename_dict'])
 
-
     def set_rename_dict(self, rename_dict):
         assert type(rename_dict) == dict, "The rename dict must be a dictionary"
         self.rename_dict = rename_dict
-
-
 
     def set_col_groups(self, groups):
         assert type(groups) == list, "The col order has to be a list of list"
@@ -155,8 +180,7 @@ class TableReg:
             assert type(group) == list, "Each col group must be a list ['name of group', first columne in the group (int), last col in group (int)]"
         self.col_groups = groups
 
-
-    def add_reg(self, reg, show_list=[], hide_list=[], blocks=[],bottom_blocks=[]):
+    def add_reg(self, reg, show_list=[], hide_list=[], blocks=[], bottom_blocks=[]):
         hide_list = hide_list + self.hide_list
         self.reg_list.append(OneReg(reg, show_list, hide_list, blocks, bottom_blocks))
 
@@ -174,15 +198,17 @@ class TableReg:
         for oneReg in self.reg_list:
             oneReg.show_list = show_list
             col.append(oneReg.create_columns())
+        try:
+            self.df = pd.concat(col, axis=1)
+        except ValueError:
+            raise ValueError("Blocks not consistent across all regressions, or mixing of regression packages")
 
-        self.df = pd.concat(col,axis=1)
-
-        self.df.columns = [r'\parboxc{c}{0.6cm}{('+str(int(i+1))+')}' for i in range(self.df.shape[1])]
+        self.df.columns = [r'\parboxc{c}{0.6cm}{(' + str(int(i + 1)) + ')}' for i in range(self.df.shape[1])]
         self.df = self.df.rename(index=self.rename_dict)
 
         self.final_show_list = show_list
         self.final_show_list = pd.Series(self.final_show_list).replace(self.rename_dict).values.tolist()
-        self.tex=''
+        self.tex = ''
 
     def create_tex(self):
         self.update_show_list()
@@ -190,41 +216,40 @@ class TableReg:
         # writing the tex modification to include name templatess
         tex = self.df.to_latex(escape=False)
         cols = tex.split('\\begin{tabular}{')[1].split('}')[0]
-        rep = list(cols.replace('l','c'))
+        rep = list(cols.replace('l', 'c'))
         rep[0] = 'l'
-        tex = tex.replace(cols,''.join(rep))
+        tex = tex.replace(cols, ''.join(rep))
 
-        if len(self.col_groups)>0:
+        if len(self.col_groups) > 0:
             # adding "group col names"
             s = '\n '
             s_line = '\n '
             for g in self.col_groups:
-                s += '& \multicolumn{'+str(1+g[2]-g[1])+'}{c}{\parboxc{c}{0.6cm}{'+g[0]+'}}'
+                s += '& \multicolumn{' + str(1 + g[2] - g[1]) + '}{c}{\parboxc{c}{0.6cm}{' + g[0] + '}}'
                 # s += '& \multicolumn{'+str(1+g[2]-g[1])+'}{c}{'+g[0]+'}'
-                s_line += r'\cmidrule(lr){'+str(g[1]+1)+'-'+str(g[2]+1)+'}'
-            s += r' \\'+'\n'
+                s_line += r'\cmidrule(lr){' + str(g[1] + 1) + '-' + str(g[2] + 1) + '}'
+            s += r' \\' + '\n'
             s_line += '\n'
 
             ts = tex.split(r'\toprule')
-            tex = ts[0]+r'\toprule' + s +s_line+ ts[1]
+            tex = ts[0] + r'\toprule' + s + s_line + ts[1]
 
             ts = tex.split(r'\midrule')
-            tex = ts[0]+r'\midrule' + ts[1]
+            tex = ts[0] + r'\midrule' + ts[1]
 
         # adding the skip between variable
         # first we extract the maxium length of a column on the first one
         L = 0
         for x in self.df.index:
-            L = max(L,len(x))
-        L+=1
-        for i in range(1,len(self.final_show_list)):
+            L = max(L, len(x))
+        L += 1
+        for i in range(1, len(self.final_show_list)):
             a = self.final_show_list[i]
             # a += ' '*(L-len(a))+'&'
             a += ' &'
             ts = tex.split(a)
             temp = ts[0][:-4] + TableReg.variable_skip + ts[0][-4:]
-            tex=temp+a+ts[1]
-
+            tex = temp + a + ts[1]
 
         # processing the group skip
         t = None
@@ -243,11 +268,11 @@ class TableReg:
         self.create_tex()
         tex = self.tex
         if TableReg.equal_lines:
-            tex=tex.replace(r'\toprule',r'\hline')
-            tex=tex.replace(r'\midrule',r'\hline')
-            tex=tex.replace(r'\bottomrule',r'\hline')
+            tex = tex.replace(r'\toprule', r'\hline')
+            tex = tex.replace(r'\midrule', r'\hline')
+            tex = tex.replace(r'\bottomrule', r'\hline')
 
-        with open(save_dir,'w') as txt:
+        with open(save_dir, 'w') as txt:
             txt.write(tex)
 
     @staticmethod
@@ -256,19 +281,17 @@ class TableReg:
         title_list = []
         for i in range(len(table_list)):
             table_list[i].create_tex()
-            title_list.append('Panel '+numbers[i]+': '+name_list[i])
-
+            title_list.append('Panel ' + numbers[i] + ': ' + name_list[i])
 
         tex = table_list[0].tex
-        temp = r' \multicolumn{6}{c}{\parboxc{c}{0.7cm}{'+title_list[i]+r'}} \\'
+        temp = r' \multicolumn{6}{c}{\parboxc{c}{0.7cm}{' + title_list[i] + r'}} \\'
         ts = tex.split(r'\toprule')
-        tex = ts[0]+r'\toprule' +temp+r'\hline'+ts[1]
-        tex = tex.replace(r'\bottomrule','')
-        tex = tex.replace(r'\end{tabular}',r'asf')
-        tex = tex.replace('\\\\\n\nasf','\\bigskip \\\\ \n')
+        tex = ts[0] + r'\toprule' + temp + r'\hline' + ts[1]
+        tex = tex.replace(r'\bottomrule', '')
+        tex = tex.replace(r'\end{tabular}', r'asf')
+        tex = tex.replace('\\\\\n\nasf', '\\bigskip \\\\ \n')
 
-
-        for i in range(1,len(table_list)):
+        for i in range(1, len(table_list)):
             t_tex = table_list[i].tex
             temp = r' \multicolumn{6}{c}{\parboxc{c}{0.6cm}{' + title_list[i] + r'}} \\'
             ts = t_tex.split(r'\toprule')
@@ -277,17 +300,12 @@ class TableReg:
             for item in t_tex.split("\n"):
                 if r'\begin{tabular}' in item:
                     t = item
-            t_tex = t_tex.replace(t,'')
-            if i+1 < len(table_list):
-                t_tex = t_tex.replace(r'\bottomrule','')
+            t_tex = t_tex.replace(t, '')
+            if i + 1 < len(table_list):
+                t_tex = t_tex.replace(r'\bottomrule', '')
                 t_tex = t_tex.replace(r'\end{tabular}', r'asf')
                 t_tex = t_tex.replace('\\\\\n\nasf', '\\bigskip \\\\ \n')
-            tex +=t_tex
+            tex += t_tex
 
-
-
-        with open(save_dir,'w') as txt:
+        with open(save_dir, 'w') as txt:
             txt.write(tex)
-
-
-
