3630c0776cc1ad89b2a5f1b18d11d3fa6c7a4b47
AntoineDidisheim <antoinedidisheim@gmail.com>
2023-12-13 17:00:10 +1100

    update the parameters as dfefault mode.

--- didipack/parameters.py
+++ didipack/parameters.py
@@ -1,53 +1,42 @@
-import numpy as np
+# basic_parameters
+# Created by Antoine Didisheim, at 06.08.19
+# job: store default basic_parameters used throughout the projects in single .py
+
+import datetime
 import itertools
+import time
+from enum import Enum
+import numpy as np
 import pandas as pd
+import socket
 import os
+import hashlib
 
 
-class RandomFeaturesParams:
-    def __init__(self):
-        self.max_rf = 50*1000
-        self.gamma_list = [0.5,0.6,0.7,0.8,0.9,1.0]
-        self.block_size_for_generation=1000
-        self.start_seed=0
-        self.voc_grid=[100, 200, 360, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000]
-        self.para_nb_of_list_group=20
-        self.para_id=0
 
 
-class TrainerParams:
-    def __init__(self):
-        self.T_train = 360
-        self.T_val = 36
-        self.testing_window = 1
-        self.shrinkage_list = np.linspace(1e-12,10,50)
-        self.save_ins = False
 
-        # this is the number of individual saving chunks.
-        # by this we mean the number of individual df contianing some oos performance that will be saved before merged.
-        # too big and we risk loosing some processing, too small and we will make a mess of the merging process.
-        self.nb_chunks = 25
-        #
-        self.min_nb_chunks_in_cluster = 10
 
-# store all parameters into a single object
+
+##################
+# params classes
+##################
+
+
+
+# store all basic_parameters into a single object
 class Params:
     def __init__(self):
-        self.name_detail = ''
+        self.name_detail = 'default'
         self.name = ''
+        self.use_hash = True
         self.seed = 12345
-        self.train = TrainerParams()
-        self.rf = RandomFeaturesParams()
 
-        self.update_model_name()
 
-    def update_model_name(self):
-        # function to overwrite
-        self.name = self.name_detail
 
     def print_values(self):
         """
-        Print all parameters used in the model
+        Print all basic_parameters used in the model
         """
         for key, v in self.__dict__.items():
             try:
@@ -57,41 +46,51 @@ class Params:
             except:
                 print(v)
 
-    def update_param_grid(self, grid_list, id_comb, verbose = False):
+        print('----', flush=True)
+
+    def update_param_grid(self, grid_list, id_comb):
         ind = []
         for l in grid_list:
             t = np.arange(0, len(l[2]))
             ind.append(t.tolist())
         combs = list(itertools.product(*ind))
-        if verbose:
-            print('comb', str(id_comb + 1), '/', str(len(combs)))
+        print('comb', str(id_comb + 1), '/', str(len(combs)))
         c = combs[id_comb]
 
         for i, l in enumerate(grid_list):
             self.__dict__[l[0]].__dict__[l[1]] = l[2][c[i]]
 
-    def save(self, save_dir, file_name='/parameters.p'):
-        # simple save function that allows loading of deprecated parameters object
-        # first we create the dir if it doesn't exists already
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
+    def finalize_parameters(self, verbose=True):
+        np.random.seed(self.seed)
+        if verbose:
+            self.update_model_name()  # automatically create a unique name for the experiment results.
+        # create a unique directory name with the basic_parameters of the experiment (the basic_parameters defines the name automatically)
+        save_dir = f'{self.model.res_dir}{self.name}/'
+        os.makedirs(save_dir, exist_ok=True)
+        # print and save the final basic_parameters
+        self.save(
+            save_dir)  # this save the param object with current configuration. So we will never forget the basic_parameters of each experiment run.
 
-
+    def save(self, save_dir, file_name='/basic_parameters.p'):
+        # simple save function that allows loading of deprecated basic_parameters object
         df = pd.DataFrame(columns=['key', 'value'])
 
         for key, v in self.__dict__.items():
             try:
                 for key2, vv in v.__dict__.items():
                     temp = pd.DataFrame(data=[str(key) + '_' + str(key2), vv], index=['key', 'value']).T
-                    df = df.append(temp)
+                    df = pd.concat([df, temp], axis=0)
+                    # df = df.append(temp)
 
             except:
                 temp = pd.DataFrame(data=[key, v], index=['key', 'value']).T
-                df = pd.concat([df,temp],axis=0)
-        df.to_pickle(save_dir + file_name)
+                df = pd.concat([df, temp], axis=0)
+                # df = df.append(temp)
+            df.to_pickle(save_dir + file_name, protocol=4)
+        # return df
 
-    def load(self, load_dir, file_name='/parameters.p'):
-        # simple load function that allows loading of deprecated parameters object
+    def load(self, load_dir, file_name='/basic_parameters.p'):
+        # simple load function that allows loading of deprecated basic_parameters object
         df = pd.read_pickle(load_dir + file_name)
         # First check if this is an old pickle version, if so transform it into a df
         if type(df) != pd.DataFrame:
@@ -102,7 +101,6 @@ class Params:
                     for key2, vv in v.__dict__.items():
                         temp = pd.DataFrame(data=[str(key) + '_' + str(key2), vv], index=['key', 'value']).T
                         df = df.append(temp)
-
                 except:
                     temp = pd.DataFrame(data=[key, v], index=['key', 'value']).T
                     df = df.append(temp)
@@ -119,7 +117,7 @@ class Params:
                     else:
                         if no_old_version_bug:
                             no_old_version_bug = False
-                            print('#### Loaded parameters object is depreceated, default version will be used')
+                            print('#### Loaded basic_parameters object is depreceated, default version will be used')
                         print('Parameter', str(key) + '.' + str(key2), 'not found, using default: ',
                               self.__dict__[key].__dict__[key2])
 
@@ -131,7 +129,31 @@ class Params:
                 else:
                     if no_old_version_bug:
                         no_old_version_bug = False
-                        print('#### Loaded parameters object is depreceated, default version will be used')
+                        print('#### Loaded basic_parameters object is depreceated, default version will be used')
                     print('Parameter', str(key), 'not found, using default: ', self.__dict__[key])
 
+    def dict_to_string_for_dir(self, d:dict, old_style =False):
+        if (self.use_hash) & (old_style==False):
+            valid_params = {k: v for k, v in d.items() if v is not None}
+            # Convert the dictionary to a string representation
+            param_string = str(valid_params)
+
+            # Create a hash of the string
+            hash_object = hashlib.sha256(param_string.encode())
+            s = hash_object.hexdigest()
+        else:
+            # the old version for backward compatibility
+            s = ''
+            for k in d.keys():
+                if d[k] is not None:
+                    # we only add to the string name if a parameters is not none. That allows us to keep compatible stuff with old models by adding new parameters with None
+                    # v= d[k] if type(d[k]) not in [type(np.array([])),type([])] else len(d[k])
+                    if type(d[k]) in [type(np.array([])), type([])]:
+                        v = len(d[k])
+                        if v == 1:
+                            v = d[k][0]
+                    else:
+                        v = d[k]
+                    s += f'{k}{v}'
+        return s
 
